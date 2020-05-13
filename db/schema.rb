e8be5378f76d91acf73ed44227d8a00ea01fdfd4
quentin natoly <quentin.natoly@gmail.com>
2020-05-13 15:31:50 +0200

    Exercice 2 completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_094749) do
+ActiveRecord::Schema.define(version: 2020_05_13_102929) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
